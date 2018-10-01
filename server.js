f724729f6f89c4db5c5c40b17a50774aef00ae4f
newmanje11 <jevonte_n@hotmail.com>
2018-10-01 07:07:51 -0400

    array up next

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 var express = require("express");
 var bodyParser = require('body-parser');
+var path = require("path");
+
+
 var app = express();
+var PORT = process.env.PORT || 8080
 
-    app.get('/', function(req,res) {
-        res.send("Hello World")
-    })
 
-    app.listen(8080);
 
-    app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
 app.use(bodyParser.json())
- 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+
+app.listen(PORT, function () {
+
+    console.log("App listening on PORT: " + PORT);
+});
